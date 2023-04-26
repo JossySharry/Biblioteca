76dd00f26ba4d2ed6342f36b3516e419e75245e2
Jossy <jsromero@pucp.pe>
2023-04-26 17:29:01 -0500

    Se agregó el proyecto

---
+++ biblioteca/src/main/java/com/egg/biblioteca/servicios/LibroServicio.java
@@ -0,0 +1,93 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.egg.biblioteca.servicios;
+
+import com.egg.biblioteca.entidades.Autor;
+import com.egg.biblioteca.entidades.Editorial;
+import com.egg.biblioteca.entidades.Libro;
+import com.egg.biblioteca.excepciones.MiException;
+import com.egg.biblioteca.repositorio.AutorRepositorio;
+import com.egg.biblioteca.repositorio.EditorialRepositorio;
+import com.egg.biblioteca.repositorio.LibroRepositorio;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+import javax.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class LibroServicio {
+    @Autowired
+    private LibroRepositorio libroRepositorio;
+    @Autowired
+    private EditorialRepositorio editorialRepositorio;
+    @Autowired
+    private AutorRepositorio autorRepositorio;
+    @Transactional
+    public void crearLibro(Long isbn, String titulo, Integer ejemplares, String idAutor, String idEditorial)throws  MiException{
+        validar(isbn, titulo, idAutor, idEditorial, ejemplares);
+        Autor autor = autorRepositorio.findById(idAutor).get();
+        Editorial editorial = editorialRepositorio.findById(idEditorial).get();
+        
+        Libro libro =   new Libro();
+        libro.setIsbn(isbn);
+        libro.setTitulo(titulo);
+        libro.setEjemplares(ejemplares);
+        libro.setAlta(new Date());
+        libro.setAutor(autor);
+        libro.setEditorial(editorial);
+        libroRepositorio.save(libro);
+        
+    }
+    public List<Libro> listarLibros(){
+        List<Libro> libros = new ArrayList();
+        libros=libroRepositorio.findAll();
+        return libros;
+    }
+    public void modificarLibro(Long isbn, String titulo, String idAutor, String idEditorial, Integer ejemplares)throws MiException{
+        validar(isbn, titulo, idAutor, idEditorial, ejemplares);
+        
+        Optional<Libro> respuesta=libroRepositorio.findById(isbn);
+        Optional<Autor> respuestaAutor=autorRepositorio.findById(idAutor);
+        Optional<Editorial> respuestaEditorial = editorialRepositorio.findById(idEditorial);
+        Autor autor=new Autor();
+        Editorial editorial=new Editorial();
+        if (respuestaAutor.isPresent()) {
+            autor = respuestaAutor.get();
+        }
+        if (respuestaEditorial.isPresent()) {
+            editorial = respuestaEditorial.get();
+            
+        }
+        if (respuesta.isPresent()) {
+            Libro libro = respuesta.get();
+            libro.setTitulo(titulo);
+            libro.setAutor(autor);
+            libro.setEditorial(editorial);
+            libro.setEjemplares(ejemplares);
+            libroRepositorio.save(libro);
+        }
+    }
+    private void validar(Long isbn, String titulo, String idAutor, String idEditorial, Integer ejemplares)throws MiException{
+        if (isbn==null) {
+            throw new MiException("El isbn no puede ser nulo");
+        }
+        if (titulo.isEmpty()|| titulo==null){
+         throw new MiException("el titulo no puede ser nulo o estar vacío");   
+        }
+        if (ejemplares==null) {
+            throw new MiException("Los ejemplares no puede ser nulo");
+        }
+        if (idAutor.isEmpty()|| idAutor==null){
+         throw new MiException("el id del autor no puede ser nulo o estar vacío");   
+        }
+        if (idEditorial.isEmpty()|| idEditorial==null){
+         throw new MiException("el id de la editorial no puede ser nulo o estar vacío");   
+        }
+    }
+}
